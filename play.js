5b537777ae8ecfe8840fb1656fe2715def3f25b1
Joacquim Mukadi <joacquim@Joacquims-MacBook-Air.local>
2021-03-10 13:59:25 -0500

    plays.js

---
+++ play.js
@@ -0,0 +1,26 @@
+
+const {connect} = require('./client.js')
+/**
+ * Establishes connection with the game server
+ */
+
+ //--- bottom connect function commented out as it was moved to client.js ----
+
+ // const connect = function() {  // creating new connection to vasily server 
+//   const conn = net.createConnection({ // setting up connection  7 - 10
+//     host: '135.23.222.131',
+//     port: 50542
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); //<---- interperates the incoming data as text
+//   conn.on('data', (x)=>{ // this is the event listenr for dat coming in 
+//     console.log(x);      // data represented in variable x // could also pass in function call
+//   })
+//   // conn.on('connect', ()=>{
+//   //   console.log('you connected');
+//   // })
+//   return conn;
+// } 
+
+console.log('Connecting ...');
+connect();
